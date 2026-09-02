77cd8e62afc492d30c500013de5dc596d04cc106
Markus Voge <sgrubsmyon@users.noreply.github.com>
2026-09-02 22:16:00 +0200

    Create extra scan route

--- src/routes/scan/+server.ts
+++ src/routes/scan/+server.ts
@@ -1,17 +1,28 @@
 import { exec } from 'child_process';
 import { promisify } from 'util';
-import { writeFile } from 'fs/promises';
+// import { writeFile } from 'fs/promises';
 import { join } from 'path';
 
 const execAsync = promisify(exec);
 
 export async function POST({ request }) {
     const formData = await request.formData();
-    const filename = formData.get('filename')?.trim() || 'scan';
+    const filename = formData.get('filename')?.toString()?.trim() || 'scan';
 
     try {
-        const outputPath = join(process.cwd(), 'static', `${filename}.pdf`);
-        const command = `scanimage --format=pdf > ${outputPath}`;
+        const outputPath = join(process.cwd(), 'static', `${filename}`);
+        // const command = `scanimage --format=pdf > ${outputPath}`;
+        const command = `
+        # 1. Alle Seiten als PNG scannen
+        scanimage --source "ADF Duplex" --resolution 300 --format png --page-height 300 \
+            --batch="${outputPath}_%03d.png"
+
+        # 2. Zu einem einzelnen PDF zusammenfügen
+        img2pdf ${outputPath}_*.png -o ${outputPath}.pdf
+
+        # 3. Temporäre PNG-Dateien aufräumen
+        rm ${outputPath}_*.png
+        `;
 
         const { stderr } = await execAsync(command);
 
