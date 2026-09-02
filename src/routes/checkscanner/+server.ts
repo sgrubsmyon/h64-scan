256cc16850cb555ef3b0975a02d24c1765a24942
Markus Voge <sgrubsmyon@users.noreply.github.com>
2026-09-02 23:49:07 +0200

    Console.log the checkscanner result

--- src/routes/checkscanner/+server.ts
+++ src/routes/checkscanner/+server.ts
@@ -9,11 +9,12 @@ const execAsync = promisify(exec);
 async function checkScannerStatus() {
     try {
         const { stdout, stderr } = await execAsync('scanimage -L');
+        console.log({ stdout, stderr });
         if (stderr) {
-            return new Response(JSON.stringify({ status: 'offline', error: stderr }), { status: 500 }); // { status: 'offline', error: stderr };
+            return new Response(JSON.stringify({ status: 'offline', error: stderr }), { status: 500 });
         }
         if (stdout.includes('No scanners were identified')) {
-            return new Response(JSON.stringify({ status: 'offline', error: 'No scanners were identified' }), { status: 500 });
+            return new Response(JSON.stringify({ status: 'offline', error: 'No scanners were identified' }), { status: 404 });
         }
         return new Response(JSON.stringify({ status: 'OK', output: stdout }), { status: 200 });
     } catch (error) {
