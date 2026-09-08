e1678d6844a9472b8574e8fda4fe9803a99a095e
Markus Voge <sgrubsmyon@users.noreply.github.com>
2026-09-09 00:07:24 +0200

    Add proper favicon and page title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<title>h64-scan</title>
 </svelte:head>
 
 {@render children()}
+
+<a href="https://www.flaticon.com/free-icons/scanner" title="scanner icons">Favicon icon created by Vectors Tank - Flaticon</a>
