49d2c2d8c11398756a6b56677c888ff6b186852f
Markus Voge <sgrubsmyon@users.noreply.github.com>
2026-09-09 00:16:43 +0200

    Update deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/bash
 
+bun --bun run build
+
 sudo install -m 644 -o root -g root ./deploy/h64-scan.service /etc/systemd/system/h64-scan.service
 
 sudo systemctl daemon-reload
-sudo systemctl start h64-scan
-sudo systemctl enable h64-scan
+
+#sudo systemctl start h64-scan
+#sudo systemctl enable h64-scan
+
+sudo systemctl restart h64-scan
+
 sudo systemctl status h64-scan
